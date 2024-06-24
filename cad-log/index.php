b907eab45709a37552b14e1f23bc71c41e4a344f
larissassk <larissasantos200702@gmail.com>
2024-06-24 09:08:00 -0300

    atualiza novo

---
+++ cad-log/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página Inicial</title>
+    <link rel="stylesheet" href="cadlog.css">
+
+</head>
+<body>
+    <h2>Bem-vindo à Página Inicial</h2>
+    <p>Você está logado com sucesso!</p>
+    <p>Seu lindo!</p>
+</body>
+</html>
