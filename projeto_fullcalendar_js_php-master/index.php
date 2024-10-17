b65bdcd18effd3cf68d8eea41ec72b50ebb4beb9
larissassk <larissasantos200702@gmail.com>
2024-10-16 22:02:20 -0300

    altera o titulo de acordo com a pagina apos ter feito alterações que busca em banco de dados eventos

--- projeto_fullcalendar_js_php-master/index.php
+++ projeto_fullcalendar_js_php-master/index.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda</title>
+    <title>Minha Agenda</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <style>
